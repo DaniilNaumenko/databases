41dc20da6f7e92512584f301d41b901ea2f5f214
DaniilNaumenko <danilanaumenko2000@yandex.ru>
2022-08-15 15:56:49 +0400

    analysis of various types of indexes

---
+++ src/main/resources/random/students_random.sql
@@ -0,0 +1,8 @@
+insert into students(name, surname, date_of_birth, phone_number, primary_skill)
+select
+    left (md5(i::text), 10),
+    left (md5(i::text), 10),
+    DATE '2020-01-01' + (random() * 700):: integer,
+    (random() * (100000 - 1) + 100000)::integer,
+    left (md5(i::text), 15)
+from generate_series(1, 100000) s(i);
